29cd346b38c0c13cd8ee3a1c0fb406044d1d92ab
Yu Wakui <wak121.y@gmail.com>
2024-11-03 15:16:14 +0900

    Initial Commit

---
+++ MotionInsight/MotionInsightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionInsightApp.swift
+//  MotionInsight
+//
+//  Created by Yu Wakui on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MotionInsightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
