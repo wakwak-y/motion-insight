1fb23290838e70d30e91512822bcc7c2cf4c176d
Yu Wakui <wak121.y@gmail.com>
2024-11-17 18:10:45 +0900

    fix: Use brew to install CocoaPods in Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,8 @@ echo "Starting ci_post_clone.sh script"
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 echo "Finished ci_post_clone.sh script"
 
+echo "CocoaPods Installing..."
+brew install cocoapods
+
 echo "Running pod install"
 pod install
