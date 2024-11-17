0859f9b0e32517794a8663127817799cf4366bef
Yu Wakui <wak121.y@gmail.com>
2024-11-17 18:05:08 +0900

    chore: Add ci_post_clone.sh for Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,3 +2,6 @@
 echo "Starting ci_post_clone.sh script"
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 echo "Finished ci_post_clone.sh script"
+
+echo "Running pod install"
+pod install
